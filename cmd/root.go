6c54b5df3e0d3e9d55df4a00cf12b819b0cc2b28
Rafael Caixeta <caixetadev@gmail.com>
2023-02-23 04:39:22 -0300

    feat: add file root

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "soccer-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Soccer-CLI: Uma ferramenta CLI para obter informações e resultados de jogos de futebol.",
+	Long: `Soccer-CLI é uma ferramenta CLI em Go que permite aos usuários obter informações atualizadas sobre as tabelas de todos os campeonatos de futebol em tempo real. Com o Soccer-CLI, os usuários podem acessar rapidamente as tabelas de seus campeonatos favoritos, bem como informações detalhadas sobre equipes, jogadores, partidas e resultados. A ferramenta é ideal para fãs de futebol que desejam acompanhar de perto os resultados de seus times e campeonatos preferidos através de uma interface de linha de comando fácil de usar.
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
