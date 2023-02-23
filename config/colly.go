217febf1bae22a51ca7aa8be7b1762403376978f
Rafael Caixeta <caixetadev@gmail.com>
2023-02-23 19:52:13 -0300

    feat: add cache

--- config/colly.go
+++ config/colly.go
@@ -8,6 +8,7 @@ func Colly() *colly.Collector {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.terra.com.br"),
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"),
+		colly.CacheDir("./.cache"),
 	)
 
 	return c
